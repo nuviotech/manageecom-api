d623e7a198f4fae2e75ed877e522e5b3978e5df5
jayhindPrajapati07 <114147639+jayhindPrajapati07@users.noreply.github.com>
2023-02-18 17:01:32 +0530

    Update ProductControllers.java

--- src/main/java/com/product/api/controllers/ProductControllers.java
+++ src/main/java/com/product/api/controllers/ProductControllers.java
@@ -16,9 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.product.api.Repo.Product2Repository;
 import com.product.api.entitys.Product2;
 
-
-//@CrossOrigin(origins = {"http://localhost:3000/","https://nuvio.in/","https://manageecom.com:8081/","https://manageecom.com:8083/","https://manageecom.com:8084/"}, maxAge = 3600)
-//@CrossOrigin(origins = {"http://localhost:3000/","https://nuvio.in/","https://manageecom.com:8001/","https://manageecom.com:8003/","https://manageecom.com:8004/"}, maxAge = 3600)
 @RestController
 public class ProductControllers {
 	@Autowired
