52e9ecb83a77101e502fc9624c28e89a0c698a0a
rk0775 <118426413+rk0775@users.noreply.github.com>
2023-02-25 14:06:10 +0530

    Image management

--- src/main/java/com/product/api/controllers/ProductControllers.java
+++ src/main/java/com/product/api/controllers/ProductControllers.java
@@ -1,5 +1,6 @@
 package com.product.api.controllers;
 
+import java.awt.Image;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,13 +15,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.product.api.Repo.Product2Repository;
+import com.product.api.entitys.Images;
 import com.product.api.entitys.Product2;
+import com.product.api.service.ImageManager;
 
 @CrossOrigin(origins = {"http://localhost:3000/","https://nuvio.in/","https://manageecom.com:8081/","https://manageecom.com:8083/","https://manageecom.com:8084/"}, maxAge = 3600)
 @RestController
 public class ProductControllers {
 	@Autowired
 	Product2Repository productRepository;
+	@Autowired
+	ImageManager imageManager;
 	
 	//get all product2s
 	@GetMapping("products")
@@ -30,7 +35,8 @@ public class ProductControllers {
 		List<Product2> ptrs=new ArrayList<>();
 		Page<Product2> page=productRepository.findAll(pageble);
 		for(Product2 p:page) {
-			ptrs.add(p);
+			p.setImages(imageManager.setTheImages(p));
+			ptrs.add(p);	
 		}
 		return ptrs.toString();
 	}
@@ -42,6 +48,7 @@ public class ProductControllers {
 		List<Product2> ptrs=new ArrayList<>();
 		Page<Product2> page=productRepository.findAll(pageble);
 		for(Product2 p:page) {
+			p.setImages(imageManager.setTheImages(p));
 			ptrs.add(p);
 		}
 		return ptrs.toString();
@@ -57,21 +64,30 @@ public class ProductControllers {
 	@GetMapping("products/{productId}")
 	public String getProductById(@PathVariable int productId) {
 		Product2 product2=productRepository.getById(productId+"");
+		product2.setImages(imageManager.setTheImages(product2));
 		return product2.toString();
 	}
 	
 	//get the product2s by categorywise (slug)
 	@GetMapping("product-categories")
 	public String getProductBySlug(@RequestParam("slug") String slug) {
-		List<Product2> ptrs=productRepository.findBySlug(slug);
+		List<Product2> ptrs=new ArrayList<>();
+		for(Product2 p:productRepository.findBySlug(slug)) {
+			p.setImages(imageManager.setTheImages(p));
+			ptrs.add(p);
+		}
 		return ptrs.toString();
 	}
 	
 	//get the product2 by brand name
 	@GetMapping("brands")
 	public String getProductByBrand(@RequestParam ("slug") String slug) {
-		List<Product2> product2s=productRepository.findByBrand(slug);
-		String str="["+product2s.toString()+"]";
+		List<Product2> ptrs=new ArrayList<>();
+		for(Product2 p : productRepository.findByBrand(slug)) {
+			p.setImages(imageManager.setTheImages(p));
+			ptrs.add(p);
+		}
+		String str="["+ptrs.toString()+"]";
 		return str;
 	}
 	
@@ -81,6 +97,7 @@ public class ProductControllers {
 		List<Product2> product2s=new ArrayList<>();
 		for(String id:ids) {
 			Product2 singleProduct=productRepository.findById(id).get();
+			singleProduct.setImages(imageManager.setTheImages(singleProduct));
 			product2s.add(singleProduct);
 		}
 		return product2s.toString();
--- src/main/java/com/product/api/entitys/Images.java
+++ src/main/java/com/product/api/entitys/Images.java
@@ -6,49 +6,22 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
-@Entity
+
 public class Images {
-	@Id
-	@GeneratedValue(strategy =GenerationType.IDENTITY)
-	int id;
-	String name;
-	String alternativeText;
+	
 	String url;
-	@ManyToOne
-	Product2 product2;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getAlternativeText() {
-		return alternativeText;
-	}
-	public void setAlternativeText(String alternativeText) {
-		this.alternativeText = alternativeText;
-	}
+	
+	
 	public String getUrl() {
 		return url;
 	}
 	public void setUrl(String url) {
 		this.url = url;
 	}
-	public Product2 getProduct() {
-		return product2;
-	}
-	public void setProduct(Product2 product2) {
-		this.product2 = product2;
-	}
+	
 	@Override
 	public String toString() {
-		return "{\"id\":" + id + ", \"name\":\"" + name + "\", \"alternativeText\":\"" + alternativeText + "\", \"url\":\"" + url
+		return "{ \"url\":\"" + url
 				+ "\" }";
 	}
 	
