809b4be4ab123a318e7b05352e6721c85e5f76df
rk0775 <118426413+rk0775@users.noreply.github.com>
2023-03-09 16:50:43 +0530

    Update MainController.java

--- src/main/java/com/product/api/controllers/MainController.java
+++ src/main/java/com/product/api/controllers/MainController.java
@@ -106,7 +106,7 @@ public class MainController {
 				salesOrder.setDate(new Date());
 				
 				SalesOrderItem  salesOrderItem=new SalesOrderItem();
-				salesOrderItem.setSku(p.getSku());
+				//salesOrderItem.setSku(p.getSku());
 				salesOrderItem.setQuantity(1);//fetch from product2
 				salesOrderItem.setSalesOrderId(100);
 				salesOrderItem.setOrderItemStatus("APPROVED");
